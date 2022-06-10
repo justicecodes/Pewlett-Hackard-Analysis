15c162c708edc5c73887e609e9c3edef8605a947
Jenna Justice <jennarjustice@gmail.com>
2022-06-10 14:12:03 -0500

    updated tables

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -6,7 +6,15 @@ CREATE TABLE departments (
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
-
+CREATE TABLE dept_emp (
+    emp_no VARCHAR(6) NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
 CREATE TABLE employees (
      emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
@@ -17,7 +25,7 @@ CREATE TABLE employees (
      PRIMARY KEY (emp_no)
 );
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -32,5 +40,13 @@ CREATE TABLE salaries (
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
+);
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+    title VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no)
 );
 SELECT * FROM departments;
