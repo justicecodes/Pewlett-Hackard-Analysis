cbadce5b64796e0a0ca9c0b018184f4e4eb2f5d9
Jenna Justice <jennarjustice@gmail.com>
2022-06-13 00:38:58 -0500

    added summary queries

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -24,7 +24,7 @@ ORDER BY rt.emp_no, rt.to_date DESC;
 
 -- C) Count by retiring titles
 SELECT COUNT(ut.title), ut.title
-INTO retiring_titles_count
+--INTO retiring_titles_count
 FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.title) DESC;
@@ -37,11 +37,36 @@ SELECT DISTINCT ON(e.emp_no) e.emp_no,
     de.from_date,
     de.to_date,
     ti.title
-INTO mentorship_eligibility
+--INTO mentorship_eligibility
 FROM employees as e
 LEFT JOIN dept_emp AS de
 ON (e.emp_no = de.emp_no)
 LEFT JOIN titles AS ti
 ON (e.emp_no = ti.emp_no)
-WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+WHERE (birth_date BETWEEN '1965-02-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no;
+
+-- SUMMARY Count of employee job titles eligible for mentorship
+SELECT COUNT(me.title),
+    me.title
+FROM mentorship_eligibility as me
+GROUP BY me.title
+ORDER BY COUNT(me.title) DESC;
+
+-- SUMMARY Determining average salary by grouping hire dates, 1980s, 1990s and 2000s
+SELECT AVG(s.salary)
+FROM salaries AS s
+INNER JOIN employees as e
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+AND (hire_date BETWEEN '2000-01-01' AND '2002-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- SUMMARY Employees listed in multiple departments
+SELECT COUNT(ti.emp_no), 
+    ti.emp_no
+FROM titles as ti
+GROUP BY ti.emp_no
+HAVING COUNT(ti.emp_no)>2;
